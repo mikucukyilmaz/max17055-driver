d8bcd7572f00423d615045b28424c0980b8ab3c7
mikucukyilmaz <mi.kucukyilmaz@gmail.com>
2022-02-16 16:49:08 +0300

    Added register addrs.

--- max17055.c
+++ max17055.c
@@ -1,16 +1,15 @@
 #include "max17055.h"
 
-static int32_t max17055_read_reg(bq2429x_ctx_t *ctx, uint8_t reg, uint8_t *data, uint16_t len)
+static int32_t max17055_read_reg(max17055_ctx_t *ctx, uint8_t reg, uint8_t *data, uint16_t len)
 {
     int32_t ret;
     ret = ctx->read_reg(ctx->handle, reg, data, len);
     return ret;
 }
 
-static int32_t max17055_write_reg(bq2429x_ctx_t *ctx, uint8_t reg, const uint8_t *data, uint16_t len)
+static int32_t max17055_write_reg(max17055_ctx_t *ctx, uint8_t reg, const uint8_t *data, uint16_t len)
 {
     int32_t ret;
     ret = ctx->write_reg(ctx->handle, reg, data, len);
     return ret;
 }
-
